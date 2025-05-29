5e70c5ff6ad302bcd3e7b02863ca7adc24c5bfd6
¨dupsy-hub¨ <dupesysdiary@gmail.com>
2025-05-29 23:31:37 +0100

    Update python app

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def get_time():
     now = datetime.now()
     current_time = now.strftime("%Y-%m-%d %H:%M:%S")
-    return f"<h1>Current Time</h1><p>{current_time}</p>"
+    return f"<h1>Current Time of the Day</h1><p>{current_time}</p>"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
